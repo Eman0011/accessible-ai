8afabdba232837e13a1c87f58545d3f8e2941adc
Eman_0011 <emanuelc@alumni.princeton.edu>
2024-08-14 00:49:47 -0400

    updating constants

--- Config.ts
+++ Config.ts
@@ -1,7 +1,7 @@
-export const ECS_CLUSTER = "AccessibleAiCdkStack-AAIECSCluster7ED08A41-ceA1U2u4kLpu"
+export const ECS_CLUSTER = "AccessibleAiCdkStack-AAIECSCluster7ED08A41-akSrEjfefK7D"
 export const ECS_TASK_DEFINITION = "AccessibleAiCdkStackAAIFargateTaskDefinition60F7F1BD"
 export const ECS_REPOSITORY = "'aai-batch-repo'"
 export const TRAINING_CONTAINER = "tpot-training";
-export const PUBLIC_VPC_SUBNET_1 = "subnet-02ca8fe230a1ff41c"
-export const PUBLIC_VPC_SUBNET_2 = "subnet-04ad9e07ca078e8f7"
+export const PUBLIC_VPC_SUBNET_1 = "subnet-0fc47c065cee9c9a2"
+export const PUBLIC_VPC_SUBNET_2 = "subnet-014b4219fe9eafbb9"
 export const TRAINING_OUTPUT_BUCKET = "model-training-output"
