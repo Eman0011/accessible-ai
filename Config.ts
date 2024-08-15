6de719159fe1567b6cb9b83df08048e849709152
Eman_0011 <emanuelc@alumni.princeton.edu>
2024-08-14 21:51:41 -0400

    Updating subnet IDs

--- Config.ts
+++ Config.ts
@@ -3,6 +3,6 @@ export const ECS_TASK_DEFINITION = "AccessibleAiCdkStackAAIFargateTaskDefinition
 export const ECS_REPOSITORY = "aai-batch-repo"
 export const ECS_SECURITY_GROUP_ID = "sg-03e1355681050c04c"
 export const TRAINING_CONTAINER = "tpot-training";
-export const PUBLIC_VPC_SUBNET_1 = "subnet-0fc47ec065cee9c9a2"
+export const PUBLIC_VPC_SUBNET_1 = "subnet-0fc47c065cee9c9a2"
 export const PUBLIC_VPC_SUBNET_2 = "subnet-014b4219fe9eafbb9"
 export const TRAINING_OUTPUT_BUCKET = "model-training-output"
