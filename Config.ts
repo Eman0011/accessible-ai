d1dab2994b70214cfb04db896e468e148e4db992
Eman_0011 <emanuelc@alumni.princeton.edu>
2024-08-23 20:02:07 -0400

    Updating batch task definition after CDK refactor

--- Config.ts
+++ Config.ts
@@ -1,5 +1,5 @@
 export const ECS_CLUSTER = "AccessibleAiCdkStack-AAIECSCluster7ED08A41-zlpXyKe6OOaV"
-export const ECS_TASK_DEFINITION = "AccessibleAiCdkStackAAIFargateTaskDefinition60F7F1BD"
+export const ECS_TASK_DEFINITION = "AccessibleAiCdkStackBatchConstructAAIFargateTaskDefinition4FD539F6"
 export const ECS_SECURITY_GROUP_ID = "sg-02a4aab6055fd6abb"
 export const PUBLIC_VPC_SUBNET_1 = "subnet-0093ac42e37923815"
 export const PUBLIC_VPC_SUBNET_2 = "subnet-0d6b049e6d2de2d7c"
