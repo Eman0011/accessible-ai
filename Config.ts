ed0cb58d4d1898d882704813637b102d28c633a4
Eman_0011 <emanuelc@alumni.princeton.edu>
2024-08-23 20:24:36 -0400

    Updating ECS security group after CDK refactor

--- Config.ts
+++ Config.ts
@@ -1,6 +1,6 @@
 export const ECS_CLUSTER = "AccessibleAiCdkStack-BatchConstructAAIECSClusterF2236B51-c2jrPCzEgdtZ"
 export const ECS_TASK_DEFINITION = "AccessibleAiCdkStackBatchConstructAAIFargateTaskDefinition4FD539F6"
-export const ECS_SECURITY_GROUP_ID = "sg-02a4aab6055fd6abb"
+export const ECS_SECURITY_GROUP_ID = "sg-019fbb585a6f3a78c"
 export const PUBLIC_VPC_SUBNET_1 = "subnet-0093ac42e37923815"
 export const PUBLIC_VPC_SUBNET_2 = "subnet-0d6b049e6d2de2d7c"
 
