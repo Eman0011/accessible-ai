433a2cf9803d71f04be0a945690a13f58d8ecf02
Eman_0011 <emanuelc@alumni.princeton.edu>
2024-11-10 23:46:53 -0500

    chore: fixup build

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,27 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['aws-sdk']
+    exclude: ['aws-sdk'],
+    include: ['react', 'react-dom']
   },
   resolve: {
     alias: {
       'core-js': 'core-js@2.6.12',
       'glob': 'glob@10.3.10'
+    }
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          aws: ['aws-amplify']
+        }
+      }
     }
   }
 })
